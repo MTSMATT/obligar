6521421f9c4a5051a88e46f3c3103303904f9322
PiedraL <joaquinlangone12@gmail.com>
2024-11-18 09:08:08 -0300

    Improving color change function

--- js/funciones.js
+++ js/funciones.js
@@ -10,37 +10,29 @@ function inicio(){
 
 let colorOriginal = true;
 function cambiarColor(){
-    let header = document.getElementById("idHeader");
-    let sectionIngresos = document.getElementById("sectionIngresos");
-    let sectionExposiciones = document.getElementById("sectionInfo");
+    let cambiarColor = document.getElementsByClassName("cambiarColor")
 
     if(colorOriginal){
-        header.style.backgroundColor = "#8DB58E";
-        sectionIngresos.style.backgroundColor = "#8DB58E";
-        sectionInfo.style.backgroundColor = "#8DB58E";
+
+        for (let i = 0; i < cambiarColor.length; i++) {
+
+            cambiarColor[i].style.backgroundColor = "#8DB58E"        
+        }
         colorOriginal = false;
-    } else{
-        header.style.backgroundColor = "#98fb98";
-        sectionIngresos.style.backgroundColor = "#98fb98";
-        sectionInfo.style.backgroundColor = "#98fb98";
+    } else if (!colorOriginal){
+        
+        for (let i = 0; i < cambiarColor.length; i++) {
+
+            cambiarColor[i].style.backgroundColor = "#98fb98"
+        }
         colorOriginal = true;
     }
 
 }
 
 function agregarArtista(){
-    /*
-    let nombre = document.getElementById("nombre").value.trim();
-    let edad = document.getElementById("edad").value;
-    let caracteristica = document.getElementById("caracteristica").value;
 
-    let artista = {nombre:nombre, edad:edad, caracteristica:caracteristica};
-    
-    let nuevoOption = document.createElement("option");
-    let listaArtistas1 = document.getElementById("idListaArtistas1");
-    listaArtistas1.appendChild(nuevoOption);
-    nuevoOption.innerHTML = artista.nombre;
-    */
+    let form = document.getElementById("formRegistrarArtistas")
 
     let nombre = document.getElementById("nombre").value.trim(); // Elimina espacios adicionales
     let edad = document.getElementById("edad").value;
@@ -56,7 +48,7 @@ function agregarArtista(){
     let opciones = listaArtistas1.options;
 
     // Verifica si el artista ya está en la lista
-    for (let i=0; i<opciones.length; i++) {
+    for (let i=0; i < opciones.length; i++) {
         if (opciones[i].text.toLowerCase() === nombre.toLowerCase()) {
             alert("El artista ya está en la lista.");
             return; // Sal del método sin agregar
@@ -68,7 +60,7 @@ function agregarArtista(){
     nuevoOption.innerHTML = nombre; // Usamos solo el nombre como texto visible
     listaArtistas1.appendChild(nuevoOption);
 
-    alert("Artista agregado exitosamente.");
+    form.reset()
 }
 
 function moverArtistaDerecha(){
